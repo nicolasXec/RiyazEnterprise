41deebd003c13796bf69fb675deb3af97c8fb015
azzyxec <azzyxec@gmail.com>
2017-05-27 19:34:43 +0530

    add scrollfix

--- app/app.js
+++ app/app.js
@@ -27,5 +27,38 @@ var vrentalApp = angular.module('webApp', [
 ]);
 
 
+
+
+angular.module('webApp').directive('scrollFix', ['$window',function($window){
+
+  var $win = angular.element($window);
+    console.log("directive intitiated first part");
+
+  return {
+    restrict: 'E',
+    link: function(scope, element, attrs){
+      var topClass = "fix-to-top";
+      var offsetTop = element.prop('offsetTop');
+
+      console.log("directive intitiated");
+
+      $win.on('scroll',function(){
+        if($window.pageYOffset >= offsetTop){
+          element.addClass(topClass);
+          console.log("topClass");
+
+        } else{
+          element.removeClass(topClass);
+            console.log("remove topClass");
+        }
+
+      });
+
+    }
+  }
+
+}]);
+
+
 // this sorta copies the contents of the config file here
 //require('./app.config.js');
