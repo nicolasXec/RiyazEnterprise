24ee2e27e614be6ab61432e78cc80d4b51133502
Azzy <azzyxec@gmail.com>
2017-05-31 13:10:32 +0530

    formatting

--- app/app.js
+++ app/app.js
@@ -8,7 +8,6 @@ require('angular-material');
 require('angular-animate');
 require('angular-aria');
 
-
 // var ngTouch = require('angular-touch');
 // var carousel  = require('angular-carousel');
 
@@ -22,13 +21,9 @@ require('./contact/contact.component.js');
 require('./storeLocations/storeLocations.component.js');
 require('./products/products.component.js');
 
-
-
-
-
 var vrentalApp = angular.module('webApp', [
   // user components
-    'homeM'
+  'homeM'
   , 'contactM'
   , 'storeLocationsM'
   , 'productsM'
@@ -40,90 +35,53 @@ var vrentalApp = angular.module('webApp', [
 ]);
 
 vrentalApp.config(['$locationProvider', '$routeProvider'
-      , function($locationProvider, $routeProvider){
+  , function ($locationProvider, $routeProvider) {
 
-   $locationProvider.hashPrefix('!');
+    $locationProvider.hashPrefix('!');
 
-   //router configs
-   $routeProvider.
-        when('/home', {
-          template: '<home></home>'
-        }).
-        when('/location', {
-           template: '<locations></locations>'
-        }).
-        when('/contact', {
-           template: '<contact></contact>'
-        }).
-        when('/product', {
-           template: '<product></product>'
-        }).
-        otherwise('/home');
+    //router configs
+    $routeProvider.
+      when('/home', {
+        template: '<home></home>'
+      }).
+      when('/location', {
+        template: '<locations></locations>'
+      }).
+      when('/contact', {
+        template: '<contact></contact>'
+      }).
+      when('/product', {
+        template: '<product></product>'
+      }).
+      otherwise('/home');
 
-  console.log('config');
+    console.log('config');
 
-}]);
+  }]);
 
-// angular.module('webApp').component('app', {
-//     template:"<body><locations></locations></body>"
-//    ,controller: [ '$scope'
-//     , function($scope ){
-//       console.log("main App controller");
-
-//       var self = this;
-
-//     //   self.navItems = [
-//     //     {name : "product", Type : true}
-//     //   , {name : "About us", Type : false}
-//     // ];
-//     // self.tittle="navItems";
-
-
-
-
-
-
-
-
-
-//     }]
-
-// })
-vrentalApp.directive('scrollFix', ['$window' ,function($window){
+vrentalApp.directive('scrollFix', ['$window', function ($window) {
 
   var $win = angular.element($window);
-    console.log("scrollfix Directive intiated");
+  console.log("scrollfix Directive intiated");
 
   return {
-    scope : {
-        fixed : '='
+    scope: {
+      fixed: '='
     }
     , restrict: 'EA'
-    , link: function($scope, element, attrs){
+    , link: function ($scope, element, attrs) {
       var topClass = "fix-to-top";
       var offsetTop = element.prop('offsetTop');
 
+      console.log("directive intitiated" + JSON.stringify($scope.fixed));
 
-
-
-
-      console.log("directive intitiated"+JSON.stringify($scope.fixed));
-
-      $win.on('scroll',function(){
-        if($window.pageYOffset >= offsetTop){
+      $win.on('scroll', function () {
+        if ($window.pageYOffset >= offsetTop) {
           element.addClass(topClass);
           $scope.fixed = true;
-
-
-
-
-
-
-        } else{
+        } else {
           element.removeClass(topClass);
-            $scope.fixed = false;
-
-
+          $scope.fixed = false;
         }
 
       });
@@ -132,153 +90,111 @@ vrentalApp.directive('scrollFix', ['$window' ,function($window){
   }
 
 }])
-.directive('navMenu', ['$window' ,function($window){
+  .directive('navMenu', ['$window', function ($window) {
 
-  var $win = angular.element($window);
-  console.log("navMenu Directive intiated");
+    var $win = angular.element($window);
+    console.log("navMenu Directive intiated");
 
-  return {
-    scope: {
-      items : '='
-    }
-    , restrict: 'EAC'
-    , controllerAs : 'ctrl'
-    , controller: ['$mdPanel' , function($mdPanel){
+    return {
+      scope: {
+        items: '='
+      }
+      , restrict: 'EAC'
+      , controllerAs: 'ctrl'
+      , controller: ['$mdPanel', function ($mdPanel) {
 
-      console.log("directive controller intiated yaa");
+        console.log("directive controller intiated yaa");
 
+        var self = this;
 
-      var self = this;
+        self._mdPanel = $mdPanel;
 
-      self._mdPanel = $mdPanel;
+        function menuController(mdPanelRef) {
+          var _self = this;
 
-      //EOC User Menu section
-      self.userMenuItems = [
-          {id:1, name:'Settings'},
-          {id:2, name:'My bookings'},
-          {id:3, name:'Help center'},
-          {id:4, name: 'Log off'}
-      ];
+          self.closeUserMenu = function () {
+            console.log("close call init");
+            _self.mdPanelRef.close()
+              .then(function () {
+                console.log("panel closed");
+                self.panelOpened = false;
+              });
 
-      function menuController(mdPanelRef){
-          var _self  = this;
-
-
-
-                  _self.click = function (item){
-                      console.log('click Event' + JSON.stringify(item));
-
-                      _self.mdPanelRef.close();
-
-                      if(item.id === 4){
-                          console.log('logout');
-                          self.logout();
-                      }
-
-                  };
-           self.closeUserMenu = function(){
-               console.log("close call init");
-              _self.mdPanelRef.close()
-                      .then(function(){
-                        console.log("panel closed");
-                        self.panelOpened = false;
-                      });
-
-           };
-
-
-
-
-
-      };
-
-      var position = self._mdPanel.newPanelPosition()
-  .relativeTo('.nav-link-btn')
-  .addPanelPosition(self._mdPanel.xPosition.CENTER, self._mdPanel.yPosition.BELOW)
-  .withOffsetY('18px');
-
-
-
-      self.userMenuConfig = {
-              attachTo: angular.element(document.getElementsByClassName("wrapper")),
-              controller: menuController,
-              controllerAs: 'ctrl',
-              templateUrl: links.templatesBasePath + 'panel.tmpl.html',
-              panelClass: 'menu-panel',
-              position: position,
-              locals: {
-                  'selected': self.selected,
-                  'userMenuItems': self.userMenuItems
-              },
-              //openFrom: ev,
-              clickOutsideToClose: true,
-              escapeToClose: true,
-              focusOnOpen: false,
-              zIndex: 4000
           };
+        };
 
-      self.selected = {selectedItem: 'Settings'};
+        var position = self._mdPanel.newPanelPosition()
+          .relativeTo('.nav-link-btn')
+          .addPanelPosition(self._mdPanel.xPosition.ALIGN_START
+                , self._mdPanel.yPosition.ALIGN_TOPS)
+          .withOffsetY('30px');
 
-      self.showUserMenu = function (ev) {
+        var panelAnimation = $mdPanel.newPanelAnimation()
+          .openFrom('.nav-link-btn')
+          .duration(280)
+          .closeTo('.nav-link-btn')
+          .withAnimation($mdPanel.animation.FADE);
 
+        self.userMenuConfig = {
+          attachTo: angular.element(document.getElementsByClassName("wrapper")),
+          controller: menuController,
+          controllerAs: 'ctrl',
+          templateUrl: links.templatesBasePath + 'panel.tmpl.html',
+          panelClass: 'menu-panel',
+          position: position,
+          animation: panelAnimation,
+          locals: {
+            'selected': self.selected,
+            'userMenuItems': self.userMenuItems
+          },
+          //openFrom: ev,
+          clickOutsideToClose: true,
+          escapeToClose: true,
+          focusOnOpen: false,
+          zIndex: 80
+        };
 
+        self.showUserMenu = function (ev, item) {
 
           self.userMenuConfig.openFrom = ev;
 
-        var panelOpen =  self._mdPanel.open(self.userMenuConfig);
-        panelOpen.then(function(){
-          self.panelOpened = true;
-          console.log("panel opened");
-        });
+          console.log('item ' + JSON.stringify(item));
 
-      };
+          // var panelOpen = self._mdPanel.open(self.userMenuConfig);
+          // panelOpen.then(function () {
+          //   self.panelOpened = true;
+          //   console.log("panel opened");
+          // });
 
+        };
 
-      self.close = function(){
+        self.close = function () {
           console.log("temporary close call");
-          if (self.panelOpened)
-          {
+          if (self.panelOpened) {
             self.closeUserMenu();
 
           }
+        }
+        //EOC user menu section
+
+      }]
+      , templateUrl: links.templatesBasePath + 'navMenu.tpl.html'
+      , link: function (scope, element, attrs, ctrl) {
+
+        // $win.on('click', function () {
+        //   console.log("window clicked yay");
+        //   ctrl.close();
+        // });
+        // $timeout(function(){
+        //             console.log(element[0].querySelector('.md-button'));
+        //            });
+        // navLink.on('mouseover',function(){
+        // console.log("on mouse over");
+        // // var offsetLeft = element[0].offsetLeft;
+        // // console.log("element offset left:"+ offsetLeft);
+        //
+        // });
       }
-
-
-
-
-
-
-      //EOC user menu section
-
-    }]
-    , templateUrl: links.templatesBasePath + 'navMenu.tpl.html'
-    , link: function(scope, element, attrs , ctrl){
-
-      $win.on('click',function(){
-        console.log("window clicked yay");
-        ctrl.close();
-      });
-      // $timeout(function(){
-      //             console.log(element[0].querySelector('.md-button'));
-      //            });
-
-
-
-    // navLink.on('mouseover',function(){
-    // console.log("on mouse over");
-    // // var offsetLeft = element[0].offsetLeft;
-    // // console.log("element offset left:"+ offsetLeft);
-    //
-    // });
-
-
     }
-  }
 
-}]);
-
-
-
-
-// this sorta copies the contents of the config file here
-//require('./app.config.js');
+  }]);
