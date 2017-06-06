8450812f9524828a60d14c0e48053981204b081e
azzyxec <azzyxec@gmail.com>
2017-06-06 15:18:16 +0530

    about us component integrated

---
+++ app/components/aboutUs/aboutUs.component.js
@@ -0,0 +1,11 @@
+var links = require("../../common/links.js");
+
+var contactModule = angular.module("aboutM", [])
+.component('aboutUs',{
+  templateUrl : links.templatesBasePath + "aboutUs.html"
+, controller : ['$scope' , function($scope){
+  console.log("about us  controler init");
+  var self = this;
+
+  }]
+});
