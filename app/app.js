43a57d98345e83c256a791fc282a4b31057f5e48
Azzy <azzyxec@gmail.com>
2017-06-06 15:27:39 +0530

    menu item added

--- app/app.js
+++ app/app.js
@@ -149,49 +149,49 @@ app.config(['$locationProvider', '$routeProvider', '$mdPanelProvider'
          },
          {id:2,
           menu: [
-          {title: "Living room",  //main group
+          {title: "Hindware",  //main group
            id:1
            , list: [
-             {title: "TABLES",  //category
+             {title: "SHOWERS",  //category
               id:1,
               products: [ //products in category
-                {name:"Dining", id:1}, {name:"study", id:2}, {name:"drawing", id:3}
+                {name:"Grand", id:1}, {name:"Multi function", id:2}, {name:"Slim rain", id:3}
               ],
               imgUrl:null},
-              {title: "SOFAS",  //category
+              {title: "BASINS",  //category
                id:2,
                products: [
-                {name:"Sectional", id:1}, {name:"Couches", id:2}, {name:"Corner", id:3}
+                {name:"Cloakroom", id:1}, {name:"corner", id:2}, {name:"vanity", id:3}
               ],
               imgUrl:"47"}
-              ,     {title: "SOFAS",  //category
+              ,     {title: "TOILETS",  //category
                    id:2,
                    products: [
-                    {name:"Sectional", id:1}, {name:"Couches", id:2}, {name:"Corner", id:3}
+                    {name:"Denver close couple", id:1}, {name:"Euro", id:2}, {name:"Savoy", id:3}
                   ],
                   imgUrl:"47"}
            ]
           , imgUrl:'45'
           },
-          {title: "Kitchen",
+          {title: "Jaqur",
             id:2
           , list: [
-             {title: "CHIMNEYS",
+             {title: "TAPS",
               id:3,
               products: [
-                {name:"Faber", id:1}, {name:"sunflame", id:2}, {name:"Kaff", id:3}
+                {name:"Edge bath", id:1}, {name:"Blade basin", id:2}, {name:"Flow deck", id:3}
               ],
               imgUrl:"42"}
            ]
           , imgUrl:'43'
           },
-          {title: "Bathroom",
+          {title: "Bayern",
           id:3
           , list: [
-             {title: "DOORS",
+             {title: "VANITY UNITS",
               id:4,
               products: [
-                {name:"Fibre", id:1}, {name:"Platic", id:2}, {name:"Rubberised", id:3}
+                {name:"Alpine", id:1}, {name:"Mino", id:2}, {name:"New polo", id:3}
               ],
               imgUrl:"45"}
            ]
