5c16948dd958d61f0ddde06f5d8fff2ec1ae71e7
azzyxec <azzyxec@gmail.com>
2017-06-07 17:14:22 +0530

    added images for carousel

--- app/app.js
+++ app/app.js
@@ -205,17 +205,74 @@ app.config(['$locationProvider', '$routeProvider', '$mdPanelProvider'
     //ECO enquiry form panel
 
 
-  }])
-  .controller('appController', [ '$anchorScroll' , function($anchorScroll){
+  }]).service('anchorSmoothScroll', function(){
+
+    this.scrollTo = function(eID) {
+
+        // This scrolling function
+        // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
+
+        var startY = currentYPosition();
+        var stopY = elmYPosition(eID);
+        var distance = stopY > startY ? stopY - startY : startY - stopY;
+        if (distance < 100) {
+            scrollTo(0, stopY); return;
+        }
+        var speed = Math.round(distance / 100);
+        if (speed >= 20) speed = 20;
+        var step = Math.round(distance / 25);
+        var leapY = stopY > startY ? startY + step : startY - step;
+        var timer = 0;
+        if (stopY > startY) {
+            for ( var i=startY; i<stopY; i+=step ) {
+                setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+            } return;
+        }
+        for ( var i=startY; i>stopY; i-=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+        }
+
+        function currentYPosition() {
+            // Firefox, Chrome, Opera, Safari
+            if (self.pageYOffset) return self.pageYOffset;
+            // Internet Explorer 6 - standards mode
+            if (document.documentElement && document.documentElement.scrollTop)
+                return document.documentElement.scrollTop;
+            // Internet Explorer 6, 7 and 8
+            if (document.body.scrollTop) return document.body.scrollTop;
+            return 0;
+        }
+
+        function elmYPosition(eID) {
+            var elm = document.getElementById(eID);
+            var y = elm.offsetTop;
+            var node = elm;
+            while (node.offsetParent && node.offsetParent != document.body) {
+                node = node.offsetParent;
+                y += node.offsetTop;
+            } return y;
+        }
+
+    };
+
+})
+  .controller('appController', [ '$anchorScroll' , 'anchorSmoothScroll' ,  function($anchorScroll , anchorSmoothScroll){
     //console.log("app conroller intiated yeay!!");
     var self = this;
 
-    self.top = function(){
+    self.top = function(eID){
       console.log('vack to top');
-      $anchorScroll();
+
+         anchorSmoothScroll.scrollTo(eID);
     }
 
+
+
   }]);
+
+
 
 app.directive('scrollFix', ['$window', function ($window) {
 
